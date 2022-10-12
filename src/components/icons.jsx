de11f2baaf4a7faaface91598fd52a679b1724b0
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2022-10-11 22:11:43 -0700

    added icons for footer, and adjusted nav bar

---
+++ src/components/icons.jsx
@@ -0,0 +1,29 @@
+import { IoDocumentText, IoLogoLinkedin, IoLogoGithub } from "react-icons/io5";
+
+export default function icons({iconName}) {
+
+    return(
+        <>
+            {iconName === "resume" ?
+                <IoDocumentText 
+                    size={"3em"}
+                    color={"#f8f8f8"}
+                />
+                :
+                <>
+                    {iconName === "github" ?
+                        <IoLogoGithub 
+                            size={"3em"}
+                            color={"#f8f8f8"}
+                        />
+                        :
+                        <IoLogoLinkedin 
+                            size={"3em"} 
+                            color={"#f8f8f8"}
+                        />
+                    }
+                </>
+            }
+        </>
+    );
+}
