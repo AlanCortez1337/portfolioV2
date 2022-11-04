cf1a73927d773a04188006e874bf531f915a07e3
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2022-11-04 12:08:47 -0700

    BEGAN UNDERSTANDING HOW TO USE THE LAYOUTS AND FETCH THE MARKDOWN FILES FOR DATA:

--- src/components/project-card.jsx
+++ src/components/project-card.jsx
@@ -2,14 +2,14 @@ import {IoLogoGithub } from "react-icons/io5/index.js";
 import { BiLinkExternal } from "react-icons/bi";
 import "../styles/card.css";
 
-export default function Card({project}) {
+export default function Card({title, gif}) {
     // need to make metadata file for project
     return(
         <div className="card">
             {/* replace it with project name metadata */}
-            <h2 className="card-title">Spotlight: ACM Team Quiz</h2>
+            <h2 className="card-title">{title}</h2>
             {/* replace it with project demo metadata */}
-            <img src="/assets/teamQuizDemo.gif" alt="spotlight" />
+            <img src={gif} alt={title} />
             <div className="card-icons">
                 {/* make into a tags that redirects using meta data */}
                 <BiLinkExternal 
---
+++ src/pages/project/acm-team-quiz.md
@@ -0,0 +1,6 @@
+---
+title: ACM TEAM QUIZ
+gif: /assets/teamQuizDemo.gif
+---
+
+This is a project :)
