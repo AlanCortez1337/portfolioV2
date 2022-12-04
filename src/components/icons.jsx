0f55d439d5bcd344fdec356f1afce04b83f911ef
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2022-12-03 21:55:13 -0800

    created the project card layout, just need to make the header and the filter mechanic

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -1,37 +1,46 @@
-import { IoDocumentText, IoLogoLinkedin, IoLogoGithub, IoArrowDownCircle } from "react-icons/io5/index.js";
+import { IoDocumentText, IoLogoLinkedin, IoLogoGithub, IoArrowDownCircle, IoArrowForwardCircle } from "react-icons/io5/index.js";
 
 export default function icons({iconName}) {
 
     return(
         <>
-            {iconName === "downArrow" ? 
-                <IoArrowDownCircle 
-                    size={"3em"}
+            {iconName === "nextArrow" ?
+                <IoArrowForwardCircle 
+                    size={"1.75em"}
                     color={"var(--icon-colors)"}
                 />
                 :
                 <>
-                    {iconName === "resume" ?
-                        <IoDocumentText 
-                            size={"3em"}
-                            color={"var(--icon-colors)"}
-                        />
-                        :
-                        <>
-                            {iconName === "github" ?
-                                <IoLogoGithub 
-                                    size={"3em"}
-                                    color={"var(--icon-colors)"}
-                                />
-                                :
-                                <IoLogoLinkedin 
-                                    size={"3em"} 
-                                    color={"var(--icon-colors)"}
-                                />
-                            }
-                        </>
-                    }
-                </>
+                {iconName === "downArrow" ? 
+                    <IoArrowDownCircle 
+                        size={"3em"}
+                        color={"var(--icon-colors)"}
+                    />
+                    :
+                    <>
+                        {iconName === "resume" ?
+                            <IoDocumentText 
+                                size={"3em"}
+                                color={"var(--icon-colors)"}
+                            />
+                            :
+                            <>
+                                {iconName === "github" ?
+                                    <IoLogoGithub 
+                                        size={"3em"}
+                                        color={"var(--icon-colors)"}
+                                    />
+                                    :
+                                    <IoLogoLinkedin 
+                                        size={"3em"} 
+                                        color={"var(--icon-colors)"}
+                                    />
+                                }
+                            </>
+                        }
+                    </>
+                }
+            </>
             }
         </>
     );
