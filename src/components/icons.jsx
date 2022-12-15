28521692a988d407e4da9b0845535fd66569e00b
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2022-12-14 19:44:02 -0800

    finished contact page, adjusted the header for project, and messed with navbar home icon

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -1,47 +1,57 @@
-import { IoDocumentText, IoLogoLinkedin, IoLogoGithub, IoArrowDownCircle, IoArrowForwardCircle } from "react-icons/io5/index.js";
+import { IoDocumentText, IoLogoLinkedin, IoLogoGithub, IoArrowDownCircle, IoArrowForwardCircle, IoMail } from "react-icons/io5/index.js";
 
 export default function icons({iconName}) {
 
     return(
         <>
-            {iconName === "nextArrow" ?
-                <IoArrowForwardCircle 
+            {iconName === "mail" ? 
+                 <IoMail 
                     size={"1.75em"}
                     color={"var(--icon-colors)"}
                 />
                 :
                 <>
-                {iconName === "downArrow" ? 
-                    <IoArrowDownCircle 
-                        size={"3em"}
-                        color={"var(--icon-colors)"}
-                    />
-                    :
-                    <>
-                        {iconName === "resume" ?
-                            <IoDocumentText 
+                    {iconName === "nextArrow" ?
+                        <IoArrowForwardCircle 
+                            size={"1.75em"}
+                            color={"var(--icon-colors)"}
+                        />
+                        :
+                        <>
+                        {iconName === "downArrow" ? 
+                            <IoArrowDownCircle 
                                 size={"3em"}
                                 color={"var(--icon-colors)"}
                             />
                             :
                             <>
-                                {iconName === "github" ?
-                                    <IoLogoGithub 
+                                {iconName === "resume" ?
+                                    <IoDocumentText 
                                         size={"3em"}
                                         color={"var(--icon-colors)"}
                                     />
                                     :
-                                    <IoLogoLinkedin 
-                                        size={"3em"} 
-                                        color={"var(--icon-colors)"}
-                                    />
+                                    <>
+                                        {iconName === "github" ?
+                                            <IoLogoGithub 
+                                                size={"3em"}
+                                                color={"var(--icon-colors)"}
+                                            />
+                                            :
+                                            <IoLogoLinkedin 
+                                                size={"3em"} 
+                                                color={"var(--icon-colors)"}
+                                            />
+                                        }
+                                    </>
                                 }
                             </>
                         }
                     </>
-                }
-            </>
+                    }
+                </>
             }
         </>
+        
     );
 }
