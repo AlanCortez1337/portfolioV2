22677426fc25f028e00da57aee07f470036ccf8a
AlanCortez1337 <acortez1337@csu.fullerton.edu>
2022-12-17 17:33:53 -0800

    added icons to nav bar, made nav bar responsive I just need to fix hamburger menu and we good

--- src/components/hamburger.jsx
+++ src/components/hamburger.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Icons from "./icons.jsx";
 import "../styles/hamburger-nav.css"
 
 
@@ -6,17 +7,26 @@ export default function Hamburger() {
 
     const [openModal, setOpenModal] = useState(false);
 
+
+    const close = (e) => {
+        e.stopPropagation();
+        setOpenModal((previous)=>!previous);
+    }
+
     return (
         <>
-            <button onClick={()=>{setOpenModal((previous)=>!previous)}}>asdfasd {openModal}</button>
-            <div onClick={()=>{setOpenModal((previous)=>!previous)}} className={openModal ? "background" : "background active"}>
+            <button onClick={()=>{setOpenModal((previous)=>!previous)}}><Icons iconName={"menu"} size="3em"/></button>
+            <div  className={!openModal ? "background" : "background active"}>
                 <div className="modal">
-                    <h1>Home</h1>
-                    <h1>About</h1>
-                    <h1>Work</h1>
-                    <h1>Contact</h1>
-                    <h1>Resume</h1>
-                    <p onClick={()=>{setOpenModal((previous)=>!previous)}}>close</p>
+                    <div className="modal-text">
+                        <a href="/"><Icons iconName={"home"} size="3em"/><h1>Home</h1></a>
+                        <a href="/about/"><Icons iconName={"abt"} size="3em"/><h1>About</h1></a>
+                        <a href="/projects/"><Icons iconName={"work"} size="3em"/><h1>Work</h1></a>
+                        <a href="/contact/"><Icons iconName={"chat"} size="3em"/><h1>Contact</h1></a>
+                        <a href="/"><Icons iconName={"resume"} size="3em"/><h1>Resume</h1></a>
+                        
+                    </div>
+                    <p onClick={close}><Icons iconName={"close"} size="3em"/></p>
                 </div>
             </div>
         </>
--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -1,55 +1,110 @@
-import { IoDocumentText, IoLogoLinkedin, IoLogoGithub, IoArrowDownCircle, IoArrowForwardCircle, IoMail } from "react-icons/io5/index.js";
+import { IoDocumentText, IoLogoLinkedin, IoLogoGithub, IoArrowDownCircle, IoArrowForwardCircle, IoMail, IoHome, IoMenu, IoBriefcase, IoChatbubbleEllipses, IoCloseCircle } from "react-icons/io5/index.js";
 
-export default function icons({iconName}) {
+export default function icons({iconName, size}) {
 
     return(
         <>
-            {iconName === "mail" ? 
-                 <IoMail 
-                    size={"1.75em"}
-                    color={"var(--icon-colors)"}
-                />
+            {iconName === "abt" ?
+                <svg width={size} height={size} viewBox="0 0 178 190" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M71.9128 10.3015C79.5952 -3.11182 98.9404 -3.11185 106.623 10.3015L175.346 130.291C181.3 140.687 176.841 153.949 165.816 158.636L97.0929 187.854C92.0927 189.979 86.4429 189.979 81.4428 187.854L12.7196 158.636C1.69443 153.949 -2.76452 140.687 3.18962 130.291L71.9128 10.3015Z" fill="#F8F8F8"/>
+                </svg>
                 :
                 <>
-                    {iconName === "nextArrow" ?
-                        <IoArrowForwardCircle 
-                            size={"1.75em"}
+                    {iconName === "home" ? 
+                        <IoHome 
+                            size={size}
                             color={"var(--icon-colors)"}
                         />
-                        :
+                    : 
                         <>
-                        {iconName === "downArrow" ? 
-                            <IoArrowDownCircle 
-                                size={"3em"}
-                                color={"var(--icon-colors)"}
-                            />
-                            :
-                            <>
-                                {iconName === "resume" ?
-                                    <IoDocumentText 
-                                        size={"3em"}
+                            {iconName === "menu" ? 
+                                <IoMenu 
+                                    size={size}
+                                    color={"var(--icon-colors)"}
+                                />
+                            : 
+                                <>
+                                {iconName === "work" ? 
+                                    <IoBriefcase 
+                                        size={size}
                                         color={"var(--icon-colors)"}
                                     />
-                                    :
+                                : 
                                     <>
-                                        {iconName === "github" ?
-                                            <IoLogoGithub 
-                                                size={"3em"}
+                                    {iconName === "chat" ? 
+                                        <IoChatbubbleEllipses 
+                                            size={size}
+                                            color={"var(--icon-colors)"}
+                                        />
+                                    : 
+                                        <>
+                                        {iconName === "close" ? 
+                                            <IoCloseCircle 
+                                                size={size}
+                                                color={"var(--icon-colors)"}
+                                            />
+                                        : 
+                                        <>
+                                        {iconName === "mail" ? 
+                                            <IoMail 
+                                                size={size}
                                                 color={"var(--icon-colors)"}
                                             />
                                             :
-                                            <IoLogoLinkedin 
-                                                size={"3em"} 
-                                                color={"var(--icon-colors)"}
-                                            />
+                                            <>
+                                                {iconName === "nextArrow" ?
+                                                    <IoArrowForwardCircle 
+                                                        size={size}
+                                                        color={"var(--icon-colors)"}
+                                                    />
+                                                    :
+                                                    <>
+                                                    {iconName === "downArrow" ? 
+                                                        <IoArrowDownCircle 
+                                                            size={size}
+                                                            color={"var(--icon-colors)"}
+                                                        />
+                                                        :
+                                                        <>
+                                                            {iconName === "resume" ?
+                                                                <IoDocumentText 
+                                                                    size={size}
+                                                                    color={"var(--icon-colors)"}
+                                                                />
+                                                                :
+                                                                <>
+                                                                    {iconName === "github" ?
+                                                                        <IoLogoGithub 
+                                                                            size={size}
+                                                                            color={"var(--icon-colors)"}
+                                                                        />
+                                                                        :
+                                                                        <IoLogoLinkedin 
+                                                                            size={size} 
+                                                                            color={"var(--icon-colors)"}
+                                                                        />
+                                                                    }
+                                                                </>
+                                                            }
+                                                        </>
+                                                    }
+                                                </>
+                                                }
+                                            </>
+                                            }
+                                            </>
                                         }
+                                        </>
+                                    }
                                     </>
                                 }
-                            </>
-                        }
-                    </>
+                                </>
+                            }
+                        </>
                     }
+                    
                 </>
+
             }
         </>
         
