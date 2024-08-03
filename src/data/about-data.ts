ce2612dbd1f294c519ea2495018c0974a9ca8be5
Alan <cortez.alan060@gmail.com>
2024-08-03 11:17:42 -0700

    isolate static data in about page

---
+++ src/data/about-data.ts
@@ -0,0 +1,48 @@
+export const skills = [
+    "HTML5",
+    "CSS3",
+    "TailwindCSS",
+    "C++",
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "NextJS",
+    "Svelte",
+    "Astro",
+    "Git",
+    "Figma",
+    "Prisma",
+];
+  
+export const hobbies = [
+    {
+        title: "Baking Bread",
+        url: "/assets/hobbies/bread.webp",
+        description:
+        "image of baked bread which Alan created to represent that baking is a hobby.",
+    },
+    {
+        title: "Partying with Parrot",
+        url: "/assets/hobbies/lua.webp",
+        description:
+        "image of my lovebird extending her neck, trying to bite the camera.",
+    },
+    {
+        title: "Dungeonin' some Dragons",
+        url: "/assets/hobbies/dnd.webp",
+        description:
+        "image of black twenty sided dice meant to play dungeons and dragons.",
+    },
+    {
+        title: "Cooking Food Stuff",
+        url: "/assets/hobbies/food.webp",
+        description:
+        "image of a bagel sandwich with a dripping runny egg that Alan made.",
+    },
+    {
+        title: "Crazily Crocheting",
+        url: "/assets/hobbies/crochet.webp",
+        description:
+        "image of a finished \"granny square\" which is a common crochet pattern.",
+    },
+];
